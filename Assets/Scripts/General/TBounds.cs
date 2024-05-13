ed916bcb32d37dd918cf969d941fd59fb196ccee
Puddle <Puddleglumstudio@gmail.com>
2024-05-13 18:31:21 -0400

    Reworked level generation

--- Assets/Scripts/General/TBounds.cs
+++ Assets/Scripts/General/TBounds.cs
@@ -1,23 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
-
+[System.Serializable]
 public class TBounds
 {
-    private float Scale;
-    
-    TBounds(float _scale)
+    public float Scale = 2;
+    public floatRange WorldX;
+    public floatRange WorldY;
+    public intRange CellX;
+    public intRange CellY;
+
+    public void SetWorld(floatRange _worldx, floatRange _worldy)
     {
+        WorldX = _worldx;
+        WorldY = _worldy;
+    }
+    public void SetCell(intRange _cellx, intRange _celly)
+    {
+        CellX = _cellx;
+        CellY = _celly;
+    }
+    public TBounds(float _scale)
+    {
+
         Scale = _scale;
     }
+
+    public TBounds(float _scale, float _sizeX, float _sizeY)
+    {
+        Scale = _scale;
+        WorldX = new floatRange(-_sizeX, _sizeX);
+        WorldY = new floatRange(-_sizeY, _sizeY);
+
+
+        CellX = new intRange(-_sizeX / Scale, _sizeX / Scale);
+        CellY = new intRange(-_sizeY / Scale, _sizeY / Scale);
+
+    }
 }
-
-
+[System.Serializable]
 
 public class floatRange
 {
-    private float min { get; }
-    private float max { get; }
+    public float min;
+    public float max;
 
     public floatRange(float _min, float _max)
     {
@@ -26,13 +53,21 @@ public class floatRange
     }
 
 }
+[System.Serializable]
+
 public class intRange
 {
-    private int min { get; }
-    private int max { get; }
+    public int min;
+    public int max;
     public intRange(int _min, int _max)
     {
         min = _min;
         max = _max;
     }
+    public intRange(float _min, float _max)
+    {
+        min = (int)_min;
+        max = (int)_max;
+    }
+
 }
--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LevelGeneration : MonoBehaviour
 {
     [SerializeField] private RoomPreset currentPreset;
+    [SerializeField] private LayerValues[] layers;
+    public bool debugSize;
 
     private void Awake()
     {
         
 
     }
-
-    private void Generaate()
+    private void Update()
     {
+        if (debugSize)
+        {
 
+            Generate();
+            debugSize = false;
+        }
+    }
+
+    private void Generate()
+    {
+        for (int i = 0; i < layers.Length; i++)
+        {
+            if (i >= currentPreset.layerValues.Length) break;
+            layers[i].L_bounds = new TBounds(currentPreset.layerValues[i].L_Scale, currentPreset.size.x ,currentPreset.size.y);
+            layers[i].L_baseTilemap.transform.localScale = Vector3.one * currentPreset.layerValues[i].L_Scale;
+        }
+            Debug.Log(layers[0].L_bounds.Scale);
     }
     private void GenerateLayer()
     {
@@ -27,4 +46,13 @@ public class LevelGeneration : MonoBehaviour
     }
 
   
+}
+[System.Serializable]
+public class LayerValues
+{
+    public TBounds L_bounds;
+    public Tilemap L_baseTilemap;
+    public Tilemap L_decorTilemap;
+    public Tilemap L_obstacleTilemap;
+
 }
