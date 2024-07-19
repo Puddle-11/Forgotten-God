cfc44eb370c477cca6dcb32bb781bf4ffdca67ff
Puddle <Puddleglumstudio@gmail.com>
2024-07-19 19:26:23 -0400

    Removed cluster enemy type

--- Assets/Scripts/Entities/Movement/OrbitalEnemyMovement.cs
+++ Assets/Scripts/Entities/Movement/OrbitalEnemyMovement.cs
@@ -7,10 +7,11 @@ public class OrbitalEnemyMovement : EntityMovement
     [Space]
     [Header("Orbital Values")]
     [Space]
-    [SerializeField] private float lerpDist;
-    protected float timer;
-    private float weight;
- 
+    [SerializeField] protected float lerpDist;
+    [SerializeField] protected float timer;
+    [SerializeField] protected float weight;
+    protected const float C = Mathf.PI * 2;
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -23,21 +24,10 @@ public class OrbitalEnemyMovement : EntityMovement
 
 
 
-        updateTimer();
+        UpdateTimer();
         base.Update();
     }
-    public virtual void updateTimer()
-    {
-        if (timer > Mathf.PI * 2)
-        {
-            timer = 0;
-        }
-        else
-        {
-            timer += Time.deltaTime * (moveSpeed / MinDist) * weight;
-        }
-
-    }
+  
     public override float CalculateSpeed()
     {
         if (Enman != null && Enman.isAlive())
@@ -53,7 +43,6 @@ public class OrbitalEnemyMovement : EntityMovement
     {
         if (target == null) return;
 
-        Vector2 TempPos = transform.position;
         if (moveSpeed > 0)
         {
 
@@ -63,31 +52,30 @@ public class OrbitalEnemyMovement : EntityMovement
             //while a distance = to min distance (aka the inner orbital circle) = 1;
             //this weight is used to calculate the average position between the target and the orbital radius
             weight = Mathf.Clamp((MinDist + lerpDist) - Vector2.Distance(transform.position, targetPos), 0, lerpDist) / lerpDist;
-            
             Vector3 lerpPos = (orbitPos * weight + (targetPos) * (1 - weight));
-         
-                TempPos = Vector2.MoveTowards(transform.position, lerpPos, currentSpeed * Time.deltaTime);
-
-            if (weight <= 0)
-            {
-                SetTimerToAngle();
-              
-             
-            }
-            transform.position = new Vector3(TempPos.x, TempPos.y, transform.position.z);
+            Vector3 tempPos = (Vector3)Vector2.MoveTowards(transform.position, lerpPos, currentSpeed * Time.deltaTime);
+            transform.position = tempPos + new Vector3(0, 0, transform.position.z);
+            //sets position to temppos
 
         }
 
     }
-    public virtual void SetTimerToAngle()
+    public virtual void UpdateTimer()
     {
-        //do sin cos calculations to get the proper angle
-        Vector3 p1 = transform.position;
-        Vector3 p2 = target.transform.position;
-        float x = p2.x - p1.x;
-        float y = p2.y - p1.y;
-        float angle = Mathf.Atan2(x, y);
-        timer = angle;
+        if (weight <= 0)
+        {
+            //do sin cos calculations to get the proper angle
+            Vector3 p1 = transform.position;
+            Vector3 p2 = target.transform.position;
+            float x = p2.x - p1.x;
+            float y = p2.y - p1.y;
+            float angle = Mathf.Atan2(x, y);
+            timer = angle;
+        }
+        else
+        {
+            timer = timer > C ? 0 : timer + Time.deltaTime * (moveSpeed / MinDist) * weight;
+        }
     }
    
 }
